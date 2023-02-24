9cde144034bd89c1446b83fb5e702daf76696688
MiguelBr12 <miguebetancur12@gmail.com>
2023-02-24 06:11:29 -0500

    Game Over Scene
    Tengo un problema con el Game Over, si bien la escena ya esta lista y los botones están enlazados para reiniciar el juego y volver al menú. No se en que parte en especifico debería poner la escena para que se mostrara luego de que el jugador pierda toda la vida.

---
+++ corgi_202301-master/Assets/Scripts/Game_Over.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Game_Over : MonoBehaviour
+{
+    public void Reiniciar()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+
+    public void Menu_GameOver()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}

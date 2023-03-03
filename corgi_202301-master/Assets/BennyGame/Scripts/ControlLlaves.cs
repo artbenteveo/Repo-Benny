4dab87e20df67cb87ae7098bf9387e9bdb91d918
Jaydee <83146834+artbenteveo@users.noreply.github.com>
2023-03-03 09:44:54 -0500

    Key Counter

---
+++ corgi_202301-master/Assets/BennyGame/Scripts/ControlLlaves.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class ControlLlaves : MonoBehaviour
+{
+    [SerializeField]
+    //H
+    private TMP_Text txt_contador_llaves;
+    private int contador_llaves;
+
+
+    public void ActualizarPuntaje(int valor)
+    {
+        contador_llaves += valor;
+        ActualizarValorUI();
+    }
+
+    // Update is called once per frame
+    protected void ActualizarValorUI()
+    {
+        txt_contador_llaves.text = "" + contador_llaves;
+    }
+}
---
+++ corgi_202301-master/Assets/BennyGame/Scripts/Llave.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MoreMountains.Tools;
+using MoreMountains.CorgiEngine;
+
+public class Llave : PickableItem
+{
+    [SerializeField]
+    private GameObject ui_puntaje;
+    [SerializeField]
+    private int valor_llave;
+    private ControlLlaves control_llaves;
+    public void Start()
+    {
+        //PickableItem.Start();
+        control_llaves = ui_puntaje.GetComponent<ControlLlaves>();
+    }
+
+    protected override void Pick(GameObject picker)
+    {
+
+        control_llaves.ActualizarPuntaje(valor_llave);
+    }
+}
